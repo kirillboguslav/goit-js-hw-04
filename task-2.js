d61cd74d573333f1142a35889caca67888bdba78
kirillboguslav <kirill.boguslav@gmail.com>
2020-05-11 17:15:47 +0300

    d/z -4

--- task-2.js
+++ task-2.js
@@ -1,1 +1,31 @@
 'use strict';
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  console.log(`Adding ${itemName} to inventory`);
+  console.log(`Removing ${itemName} from inventory`);
+  action.call(inventory, itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
